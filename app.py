cb4aa05978e7b3d090bd43504b68f8ea3f01e290
Kabiru Haruna <kabiruharuna@Kabirus-MacBook-Pro.local>
2025-05-01 16:21:13 +0100

    implement auth

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 import secrets
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -29,6 +30,55 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "27585416639052739758850893141864765218"
     jwt = JWTManager(app)
 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+    
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+    
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token is not fresh.", "error": "fresh_token_required"}
+            ),
+            401
+        )
+    
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401
+        )
+    
+    @jwt.expired_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({"message": "Signature verification failed.", "error": "invalid_token"}),
+            401
+        )
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({"description": "Request does not contain an access token.", "error": "authorization_required"}),
+            401
+        )
+
     with app.app_context():
         db.create_all()
 
@@ -39,126 +89,6 @@ def create_app(db_url=None):
 
 
     return app
-
-
-# stores = [
-#     {
-#         "name": "my Store",
-#         "items": [
-#             {
-#                 "name": "Chair",
-#                 "price": 15.99
-#             }
-#         ]
-#     }
-# ]
-
-# stores = {}
-# items = {}
-
-
-# @app.get("/store")
-# def get_stores():
-#     return {"stores": list(stores.values())}
-
-
-# @app.post("/store")
-# def create_store():
-#     store_data = request.get_json()
-#     if "name" not in store_data:
-#         abort(
-#             400,
-#             message="Bad request. Ensure 'name' is included in the JSON payload."
-#         )
-
-#     for store in stores.values():
-#         if store_data["name"] == store["name"]:
-#             abort(400, message=f"Store already exists.")
-#     store_id = uuid.uuid4().hex
-#     store = {**store_data, "id": store_id}
-#     stores[store_id] = store
-#     return store, 201
-
-# @app.get("/store/<string:storee_id>")
-# def get_store(storee_id):
-#     try:
-#         return stores[storee_id]
-#     except KeyError:
-#         abort(404, message="Store not found")
-
-
-
-# @app.post("/item")
-# def create_item():
-#     item_data = request.get_json()
-#     if(
-#         "price" not in item_data
-#         or "store_id" not in item_data
-#         or "name" not in item_data
-#     ):
-#         abort(
-#             400,
-#             message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
-#         )
-
-#     for item in items.values():
-#         if(
-#             item_data["name"] == item["name"]
-#             and item_data["store_id"] == item["store_id"]
-#         ):
-#             abort(400, message=f"Item already exists")
-        
-#     if item_data["store_id"] not in stores:
-#         abort(404, message="Item not found")
- 
-#     item_id = uuid.uuid4().hex
-#     item = {**item_data, "id": item_id}
-#     items[item_id] = item
-#     return item, 201
-
-# @app.get("/item")
-# def get_all_items():
-#     return {"items": list(items.values())}
-
-
-# @app.get("/item/<string:item_id>")
-# def get_item(item_id):
-#     try:
-#         return items[item_id]
-#     except KeyError:
-#         abort(404, message="Item not found")
-
-# @app.delete("/item/<string:item_id>")
-# def delete_item(item_id):
-#     try:
-#         del items[item_id]
-#         return {"message": "Item deleted"}
-#     except KeyError:
-#         abort(404, message="Item not found")
-
-
-# @app.delete("/store/<string:store_id>")
-# def delete_store(store_id):
-#     try:
-#         del stores[store_id]
-#         return {"message": "Store deleted"}
-#     except KeyError:
-#         abort(404, message="Store not found")
-
-
-
-# @app.put("/item/<string:item_id>")
-# def update_item(item_id):
-#     item_data = request.get_json()
-#     if "price" not in item_data or "name" not in item_data:
-#         abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
-#     try:
-#         item = items[item_id]
-#         item |= item_data
-#         return item  
-#     except KeyError:
-#         abort(404, message="Item not found")
-    
 
 ### start app
 # create the docker image -> docker build -t flask-smorest-api .
